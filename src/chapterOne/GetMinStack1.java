ac5ef3b4f42966d347343ea8b418c2941139f719
guangxush <guangxush@163.com>
2018-05-27 14:26:05 +0800

    2018-5-27

---
+++ src/chapterOne/GetMinStack1.java
@@ -0,0 +1,43 @@
+package chapterOne;
+import java.util.Stack;
+/**
+ * 设计一个有getMing功能的栈
+ * @author Console
+ *
+ */
+public class GetMinStack1 {
+	private Stack<Integer> stackData;
+	private Stack<Integer> stackMin;
+	public GetMinStack1() {
+		this.stackData = new Stack<Integer>();
+		this.stackMin  = new Stack<Integer>();
+	}
+	//元素入栈
+	public void push(int newNum) {
+		if(this.stackMin.isEmpty()) {
+			this.stackMin.push(newNum);
+		}else if(newNum<=this.getMin()) {
+			this.stackMin.push(newNum);
+		}
+		this.stackData.push(newNum);
+	}
+	//获取最小元素
+	public int getMin() {
+		if(this.stackData.isEmpty()) {
+			throw new RuntimeException("Your stack is empty");
+		}else {
+			return stackMin.peek();
+		}
+	}
+	//元素出栈
+	public int pop() {
+		if(this.stackMin.isEmpty()) {
+			throw new RuntimeException("Your stack is empty");
+		}
+		int value = this.stackData.pop();
+		if(value==this.getMin()) {
+			this.stackMin.pop();
+		}
+		return value;
+	}
+}
--- src/chapterOne/README.md
+++ src/chapterOne/README.md
@@ -1,3 +1,4 @@
-# Stack 
-
-# Queue
+- [GetMinStack1](https://github.com/guangxush/JavaDataStructure/blob/master/src/chapterOne/GetMinStack1.java) 得到栈中最小元素
+- [StackReverse](https://github.com/guangxush/JavaDataStructure/blob/master/src/chapterOne/StackReverse.java) 使用递归函数和栈操作逆序一个栈
+- [TwoStacksQueue](https://github.com/guangxush/JavaDataStructure/blob/master/src/chapterOne/TwoStacksQueue.java) 两个栈实现队列
+- [PetEnterQueue](https://github.com/guangxush/JavaDataStructure/blob/master/src/chapterOne/PetEnterQueue.java) 猫狗队列
