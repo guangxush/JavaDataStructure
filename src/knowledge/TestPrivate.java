9b74fd47f0ee091f59fa5279c3a762af0c8e4d84
guangxush <guangxush@163.com>
2018-11-06 21:33:07 +0800

    test private

--- src/knowledge/TestPrivate.java
+++ src/knowledge/TestPrivate.java
@@ -4,7 +4,6 @@ import junit.framework.TestCase;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class TestPrivate {
