42b6893c2a64edd310a67e54cd65e856025be8a1
gshan <guangxush@163.com>
2018-11-11 21:03:22 +0800

    tree

--- src/codereview/BianryTreeTraversal.java
+++ src/codereview/BianryTreeTraversal.java
@@ -35,14 +35,73 @@ public class BianryTreeTraversal {
         while(!queue.isEmpty()){
             Node temp = queue.remove();
             System.out.print(temp.value+" ");
-            if(head.left!=null) queue.add(head.right);
-            if(head.right!=null) queue.add(head.left);
+            if(temp.left!=null) queue.add(head.right);
+            if(temp.right!=null) queue.add(head.left);
         }
     }
 
+    //深度优先遍历
+    public static void depthTraversal(Node head){
+        Stack<Node> stack = new Stack<>();
+        stack.push(head);
+        while(!stack.isEmpty()){
+            Node temp = stack.pop();
+            System.out.println(temp.value);
+            if(temp.right!=null) stack.push(head.right);
+            if(temp.left!=null) stack.push(head.left);
+        }
+    }
+
+    //先序遍历非递归实现
+    public static void preOrderUnrecur(Node head){
+        if(head==null) return;
+        Stack<Node> stack = new Stack<>();
+        stack.add(head);
+        while(!stack.isEmpty()){
+            Node temp = stack.pop();
+            System.out.print(temp+" ");
+            if(temp.right!=null) stack.push(temp.right);
+            if(temp.left!=null) stack.push(temp.left);
+        }
+    }
+
+    //中序遍历非递归实现
+    public static void inOrderUnRecur(Node head){
+        if(head==null) return;
+        Stack<Node> stack = new Stack<>();
+        while(!stack.isEmpty()||head!=null){
+            if(head!=null){
+                stack.push(head);
+                head = head.left;
+            }else{
+                head = stack.pop();
+                System.out.print(head.value+" ");
+                head = head.right;
+            }
+        }
+    }
+
+    //后序遍历非递归实现
+    public static void posOrderUnRecur(Node head){
+        if(head==null) return;
+        Stack<Node> stack = new Stack<>();
+        stack.push(head);
+        Node temp = null;
+        while(!stack.isEmpty()){
+            temp = stack.peek();
+            if(temp.left!=null&&head!=temp.left&&head.right!=temp.right){
+                stack.push(temp.left);//左子树还没有打印完毕
+            }else if(temp.right!=null&&head!=temp.right){
+                stack.push(temp.right);//右子树还没有处理过
+            }else{
+                //左右子树都处理完毕
+                System.out.print(stack.pop().value+" ");
+                head = temp;
+            }
+        }
+    }
 
     //构建一个二叉树
-
     /**
      *     1
      *    2 3
@@ -67,6 +126,11 @@ public class BianryTreeTraversal {
         node3.right = node7;
         node4.left = node8;
         node4.right = node9;
+        node5.left = node5.right = null;
+        node6.left = node6.right = null;
+        node7.left = node7.right = null;
+        node8.left = node8.right = null;
+        node9.left = node9.right = null;
         return node1;
     }
 
@@ -80,8 +144,12 @@ public class BianryTreeTraversal {
         System.out.println();
         System.out.println("posOrderRecur:");
         posOrderRecur(head);
+        System.out.println();
         System.out.println("levelOrderTraversal");
         levelOrderTraversal(head);
+        System.out.println();
+        System.out.println("depthTraversal");
+        depthTraversal(head);
         System.out.println();
     }
 }
