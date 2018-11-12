417e816bb9c2cfbae43d3ccd0ff4d827af009bce
guangxush <guangxush@163.com>
2018-11-12 23:00:39 +0800

    equal ==

--- src/codingInterviews/LearnString.java
+++ src/codingInterviews/LearnString.java
@@ -6,13 +6,18 @@ public class LearnString {
 		String b = "hello";
 		String c = b;
 		String d = new String("hello");//存放在堆内存中
-		System.out.println(a==b);
-		System.out.println(b==c);
-		System.out.println(a==c);
-		System.out.println(a==d);
-		System.out.println(a.equals(b));
-		System.out.println(a.equals(c));
-		System.out.println(c.equals(b));
-		System.out.println(a.equals(b));//==是比较地址，equals()比较对象内容。
+		System.out.println(a==b);//true
+		System.out.println(b==c);//true
+		System.out.println(a==c);//true
+		System.out.println(a==d);//false
+		System.out.println(a.equals(b));//true
+		System.out.println(a.equals(c));//true
+		System.out.println(c.equals(b));//true
+		System.out.println(a.equals(b));//true
+		//==是比较地址，equals()比较对象内容。
+		String A = new String("hello");
+		String B = new String("hello");
+		System.out.println(A.equals(B));//true
+		System.out.println(A==B);//false
 	}
 }
