18433d5bd3662ec6909f149f9db341be3d20e8dc
guangxush <guangxush@163.com>
2018-11-18 23:21:13 +0800

    dp

--- src/codereview/DPtest.java
+++ src/codereview/DPtest.java
@@ -5,4 +5,23 @@ package codereview;
  */
 public class DPtest {
     //计算最大子段和
+    public static int findGreatestSubArray(int[] a){
+        if(a==null) return 0;
+        int sum = 0;
+        int max = 0;
+        for(int i=0;i<a.length;i++){
+            if(sum<=0)
+                sum = a[i];
+            else
+                sum += a[i];
+            if(sum>max)
+                max = sum;
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] array={1,-2,3,10,-4,7,2,-5};
+        System.out.println(findGreatestSubArray(array));
+    }
 }
